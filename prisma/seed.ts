b6ff0b9b7a66c10a0ed19100d47343e31978babf
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-11-10 15:21:50 +0100

    added txt file with commands for ogr2ogr geojson add

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,9 +4,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Delete all `User` and `Message` records
-  await prisma.message.deleteMany({});
-  await prisma.user.deleteMany({});
   // (Re-)Create dummy `User` and `Message` records
   await prisma.user.create({
     data: {
