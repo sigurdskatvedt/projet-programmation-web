3168b363edb7bf4d0791d31558de3f6743fed751
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-11-28 18:33:56 +0100

    git diverge fix

---
+++ components/best_players.tsx
@@ -0,0 +1,5 @@
+import "./layoutstyle.css";
+
+export default function BestPlayers() {
+  return <h1 className="bestp">Best players</h1>;
+}
--- components/main_ui/top.tsx
+++ components/main_ui/top.tsx
@@ -1,19 +1,27 @@
-import GeoR from "./georesto";
-import Rectang from "./rect";
+import "./layoutstyle.css"
+import GeoR from "./georesto"
+import Rectang from "./rect"
+import BestPlayers from "./best_players"
+import CrownImage from "./crown_image"
+
 
 export default function TopComp() {
-  return (
-    <div className="top">
-      <GeoR />
-      <Rectang idNumber={"rectangle1"} />
-      <Rectang idNumber={"rectangle2"} />
-      <Rectang idNumber={"rectangle3"} />
-      <Rectang idNumber={"rectangle4"} />
-      <Rectang idNumber={"rectangle5"} />
-      <Rectang idNumber={"rectangle6"} />
-      <Rectang idNumber={"rectangle7"} />
-      <Rectang idNumber={"rectangle8"} />
-      <Rectang idNumber={"rectangle9"} />
-    </div>
-  );
-}
+    return (
+        <div className="top">
+        <GeoR />
+        <CrownImage />
+        <BestPlayers />
+        <Rectang idNumber={"rectangle1"}/>
+        <Rectang idNumber={"rectangle2"}/>
+        <Rectang idNumber={"rectangle3"}/>
+        <Rectang idNumber={"rectangle4"}/>
+        <Rectang idNumber={"rectangle5"}/>
+        <Rectang idNumber={"rectangle6"}/>
+        <Rectang idNumber={"rectangle7"}/>
+        <Rectang idNumber={"rectangle8"}/>
+        <Rectang idNumber={"rectangle9"}/>
+        
+        </div>
+    )
+  }
+
